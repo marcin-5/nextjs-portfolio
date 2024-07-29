5c1f0ea41fb45459728072e5ff32449a90f0bc6a
Marcin Bojara <marcin_5@mail.ru>
2024-07-29 17:53:59 +0200

    Add Card Component to UI
    Introduced a new `Card` component in `components/ui/card.tsx` with customizable title and content areas. Updated `Home` page to utilize the `Card` component, enhancing the UI by providing a styled and encapsulated container for content.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 "use client";
 
+import Card from "@/components/ui/card";
 import WaterWaveWrapper from "@/components/visualEffects/water-wave-wrapper";
 
 export default function Home() {
   return (
     <WaterWaveWrapper imageUrl={""} dropRadius={3} perturbance={3} resolution={1536}>
-      {() => <div className={"h-screen"}></div>}
+      {() => (
+        <div className={"w-full p-10"}>
+          <div className={"max-w-2xl mx-auto"}>
+            <Card title={"UI Components"}>...</Card>
+          </div>
+        </div>
+      )}
     </WaterWaveWrapper>
   );
 }
