86704f8f720f23c119f53546ebf62a37539f47e8
Marcin Bojara <marcin_5@mail.ru>
2024-08-03 18:18:45 +0200

    Refactor Home component layout
    Replaced individual UI components and elements with a single LandingSection component. Cleaned up imports to remove redundant UI elements and icons, simplifying the overall codebase and enhancing maintainability.

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,14 @@
 "use client";
 
-import Button from "@/components/ui/button";
-import Card from "@/components/ui/card";
-import FancyButton from "@/components/ui/fancy-button";
-import Input from "@/components/ui/input";
-import LiveClock from "@/components/ui/live-clock";
-import Profile from "@/components/ui/profile";
-import ScrollDown from "@/components/ui/scroll-down";
-import TextArea from "@/components/ui/text-area";
-import MagneticWrapper from "@/components/visualEffects/magnetic-wrapper";
 import WaterWaveWrapper from "@/components/visualEffects/water-wave-wrapper";
-import {FaArrowRight} from "react-icons/fa";
-import {FaUserLarge} from "react-icons/fa6";
-import {IoHome} from "react-icons/io5";
+import LandingSection from "@/sections/landing";
 
 export default function Home() {
   return (
     <WaterWaveWrapper imageUrl={""} dropRadius={3} perturbance={3} resolution={1536}>
       {() => (
-        <div className={"w-full p-10"}>
-          <div className={"max-w-2xl mx-auto"}>
-            <Card title={"UI Components"}>
-              <div className="grid grid-cols-4">
-                <Button>Basic button</Button>
-                <Button><IoHome/>Basic button</Button>
-                <Button isIcon><FaUserLarge/></Button>
-                <Button link={"http://www.google.com"}>Google</Button>
-              </div>
-              <Input type={"text"} placeholder={"Full name"}/>
-              <Input type={"text"} placeholder={"User name"} icon={<FaUserLarge/>}/>
-              <TextArea placeholder={"description"}/>
-              <TextArea placeholder={"description"} icon={<FaUserLarge/>}/>
-              <Profile/>
-              <MagneticWrapper className={"w-[350px]"}>
-                <FancyButton text={"Contact me"} icon={<FaArrowRight/>}/>
-              </MagneticWrapper>
-              <LiveClock timeZone={"Europe/Warsaw"}/>
-              <MagneticWrapper>
-                <ScrollDown/>
-              </MagneticWrapper>
-            </Card>
-          </div>
+        <div className={"pb-8"}>
+          <LandingSection/>
         </div>
       )}
     </WaterWaveWrapper>
