d10f7dbe1685e134d584a4271a223026ae873f60
Marcin Bojara <marcin_5@mail.ru>
2024-08-04 22:21:17 +0200

    Add FeaturedSection to homepage
    Created a new FeaturedSection component and integrated it into the homepage layout, positioned below the LandingSection. This update enhances the visual presentation by highlighting featured work.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import WaterWaveWrapper from "@/components/visualEffects/water-wave-wrapper";
+import FeaturedSection from "@/sections/featured";
 import LandingSection from "@/sections/landing";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       {() => (
         <div className={"pb-8"}>
           <LandingSection/>
+          <FeaturedSection/>
         </div>
       )}
     </WaterWaveWrapper>
