e027bff69920c8ad26d301c3ad7928257969498e
Marcin Bojara <marcin_5@mail.ru>
2024-08-11 19:33:26 +0200

    Add MeCard component with image and tag display
    Created a new MeCard component featuring a background image and tags. This component uses a reusable TagComponent for displaying text tags over the image.

---
+++ components/cards/me.tsx
@@ -0,0 +1,33 @@
+import Card from "@/components/ui/card";
+import {mergeClassNames} from "@/lib/utils";
+import MyImg from "@/public/assets/images/gallery/me1.jpg";
+import Image from "next/image";
+
+export default function MeCard() {
+  return (
+    <Card className="2xl:h-full">
+      <div className="w-full h-[400px] sm:h-[500px] overflow-hidden">
+        {/* background image */}
+        <Image
+          src={MyImg}
+          alt="marcin bojara"
+          className="absolute top-0 left-0 bottom-0 right-0 h-full w-full object-cover"
+        />
+        {/* Tags */}
+        <div className="absolute top-[75%] space-y-2">
+          <TagComponent text="Hello, universe 👋"/>
+          <TagComponent text="I'm Marcin Bojara"/>
+          <TagComponent text="Full stack developer"/>
+        </div>
+      </div>
+    </Card>
+  );
+}
+
+const TagComponent = ({text, className}: { text: string; className?: string }) => {
+  return (
+    <div className={mergeClassNames("bg-black/[0.7] w-fit py-1.5 px-3 rounded-2xl", className)}>
+      <p className="text-primary-foreground leading-[110%] font-bold">{text}</p>
+    </div>
+  );
+};
