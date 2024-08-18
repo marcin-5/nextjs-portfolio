2139df85fda1ffe903cf72eaf9642f79eecbdcd9
Marcin Bojara <marcin_5@mail.ru>
2024-08-18 19:05:05 +0200

    Add gallery data module
    Created a new module in data/gallery.ts to manage gallery images. Additionally, updated data/index.ts to export the gallery images for easier access throughout the application.

---
+++ data/gallery.ts
@@ -0,0 +1,15 @@
+import Img0 from "@/public/assets/images/gallery/me1.jpg";
+import Img1 from "@/public/assets/images/gallery/me2.jpg";
+
+const galleryImages = [
+  {
+    id: 0,
+    img: Img0,
+  },
+  {
+    id: 1,
+    img: Img1,
+  },
+];
+
+export default galleryImages;
---
+++ data/index.ts
@@ -0,0 +1,3 @@
+import galleryImages from "./gallery";
+
+export {galleryImages};
