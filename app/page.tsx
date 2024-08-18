a86413e2dc0980e7f4fd4eb31b02e6162ed253b3
Marcin Bojara <marcin_5@mail.ru>
2024-08-18 16:08:35 +0200

    Remove WaterWaveWrapper and add AboutSection
    Eliminated the `WaterWaveWrapper` component from the landing page and replaced it with a simple fragment wrapper. Added `AboutSection` to enhance the homepage content. Updated `pnpm-lock.yaml` and `package.json` to remove unused dependencies related to `react-water-wave`.

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,15 @@
 "use client";
 
-import WaterWaveWrapper from "@/components/visualEffects/water-wave-wrapper";
+import AboutSection from "@/sections/about";
 import FeaturedSection from "@/sections/featured";
 import LandingSection from "@/sections/landing";
 
 export default function Home() {
   return (
-    <WaterWaveWrapper imageUrl={""} dropRadius={3} perturbance={3} resolution={1536}>
-      {() => (
-        <div className={"pb-8"}>
-          <LandingSection/>
-          <FeaturedSection/>
-        </div>
-      )}
-    </WaterWaveWrapper>
+    <>
+      <LandingSection />
+      <FeaturedSection />
+      <AboutSection />
+    </>
   );
 }
