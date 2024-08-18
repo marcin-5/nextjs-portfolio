0b5062f3f5a051c0d3d465e59c77283bce152b86
Marcin Bojara <marcin_5@mail.ru>
2024-08-18 16:23:25 +0200

    Wrap Home component content in a div.
    Replace the fragment with a div having a padding-bottom class in the Home component. This ensures consistent spacing at the bottom of the page sections.

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,10 @@ import LandingSection from "@/sections/landing";
 
 export default function Home() {
   return (
-    <>
+    <div className={"pb-8"}>
       <LandingSection />
       <FeaturedSection />
       <AboutSection />
-    </>
+    </div>
   );
 }
