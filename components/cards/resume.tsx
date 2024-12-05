f951bc72073262be99c6f1c094b8964103b3eaf0
Marcin Bojara <marcin_5@mail.ru>
2024-12-05 19:32:24 +0100

    Add dynamic sentence generation to ResumeCard
    Introduced a function to generate dynamic sentence content for the ResumeCard component using bit manipulation. This change enhances the card by providing varied text combinations on each render, creating a more engaging and personalized experience for users.

--- components/cards/resume.tsx
+++ components/cards/resume.tsx
@@ -3,24 +3,49 @@ import Card from "@/components/ui/card";
 import Socials from "@/components/ui/socials";
 import SignatureImg from "@/public/assets/images/me/signature.png";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import { FaDownload } from "react-icons/fa";
 
+const RESUME = [
+  [
+    "A long-time enthusiast of coding, I've spent years exploring the world of programming. My skills encompass both front-end and back-end development.",
+    "With years dedicated to the craft of coding, I possess a robust skillset encompassing both front-end and back-end development.",
+  ],
+  [
+    "I'm always eager to learn more.  I find new concepts readily grasped and enjoy the challenge of problem-solving. I'm adept at finding efficient solutions.",
+    "My aptitude for rapid learning and efficient problem-solving allows me to readily adapt to new challenges.",
+  ],
+  [
+    "A commitment to robust web security underpins my approach, and I am eager to collaborate on innovative projects that redefine online possibilities.",
+    "I'm particularly interested in ensuring robust web security.  Excited to collaborate on projects that push creative boundaries online, I'm ready to contribute my skills.",
+  ],
+];
+const BIT_SHIFT = Math.pow(2, RESUME.length);
+
+function getSentencesBasedOnBits(randomBitSelector: number) {
+  return (
+    RESUME.map((sentencePair, index) => {
+      const bitValue = (randomBitSelector >> index) & 1;
+      return sentencePair[bitValue];
+    }).join(" ") + " 🚀🖥"
+  );
+}
+
 export default function ResumeCard() {
+  const [sentence, setSentence] = useState("");
+
+  useEffect(() => {
+    const randomBitSelector = Math.floor(Math.random() * BIT_SHIFT);
+    setSentence(getSentencesBasedOnBits(randomBitSelector));
+  }, []);
+
   return (
     <Card className="md:h-full">
-      <p className="text-lg xl:text-2xl font-medium text-primary-foreground text-justify">
-        Your web wizard and digital handyman. Armed with years of tinkering in the programming playground, I&apos;ve got
-        a toolbox overflowing with front-end sparkles and back-end might. Add a bit of AI cleverness and a lock-tight
-        dedication to web security, and you&apos;ve got a recipe for online grandeur. Ready for a little internet magic?
-        Let&apos;s make your web dreams a screen reality! 🚀🖥
-      </p>
-      {/*Signature*/}
+      <p className="text-lg xl:text-2xl font-medium text-primary-foreground text-justify">{sentence}</p>
       <div className={"flex items-end justify-end pb-8"}>
         <Image src={SignatureImg} alt="marcin bojara" />
       </div>
-      {/*Socials and resume btn*/}
       <div className={"flex items-center justify-between md:absolute md:bottom-6 md:left-6 md:w-[calc(100%-48px)]"}>
-        {/*Socials*/}
         <Socials />
         <Button>
           <FaDownload />
