4bc826cbd719833f6ef205793420634e982f45ff
Marcin Bojara <marcin_5@mail.ru>
2025-02-14 19:09:23 +0100

    Update slogan content and improve code formatting
    Revised the slogan text to present a more concise, imaginative message. Additionally, applied consistent formatting and indentation for better readability and maintainability of the code. No functionality was altered.

--- sections/landing.tsx
+++ sections/landing.tsx
@@ -4,44 +4,44 @@ import ScrollDown from "@/components/ui/scroll-down";
 import MagneticWrapper from "@/components/visualEffects/magnetic-wrapper";
 
 export default function LandingSection() {
-  const renderSlogan = () => (<div
-    className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mt-8 leading-[14vw] lg:leading-[11vw] 2xl:leading-[9rem] font-medium h-[40rem]">
-    <div
-      className="flex flex-col justify-center items-center text-primary-foreground text-[17vw] lg:text-[14vw] 2xl:text-[12rem] uppercase tracking-[-0.3rem]">
-      <div>
-        <span>Code</span>
-      </div>
-      <div>
-        <span>Crafting</span>
-      </div>
-      <div className="relative">
-        <span>Cleverness</span>
-        <div
-          className="text-[1rem] leading-[1.4rem] tracking-normal absolute top-[15vw] lg:top-[11.5vw] 2xl:top-[10rem] left-0 2xl:left-[50rem] w-[30rem] uppercase font-normal">
-          <span>Empowering innovations</span>
-          <br/>
-          <span>through inspired design</span>
-          <br/>
-          <span>where challenges trigger creativity</span>
-          <br/>
-          <span>and solutions define capabilities anew.</span>
+  const renderSlogan = () => (
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mt-8 leading-[14vw] lg:leading-[11vw] 2xl:leading-[9rem] font-medium h-[40rem]">
+      <div className="flex flex-col justify-center items-center text-primary-foreground text-[17vw] lg:text-[14vw] 2xl:text-[12rem] uppercase tracking-[-0.3rem]">
+        <div>
+          <span>Code</span>
+        </div>
+        <div>
+          <span>Crafting</span>
+        </div>
+        <div className="relative">
+          <span>Cleverness</span>
+          <div className="text-[1rem] leading-[1.4rem] tracking-normal absolute top-[15vw] lg:top-[11.5vw] 2xl:top-[10rem] left-0 2xl:left-[50rem] w-[30rem] uppercase font-normal">
+            <span>Imaginative design approach</span>
+            <br />
+            <span>sparks creativity,</span>
+            <br />
+            <span>turning challenges into solutions</span>
+            <br />
+            <span>that unlock potential.</span>
+          </div>
         </div>
       </div>
     </div>
-  </div>);
+  );
 
-  return <div className={"relative h-screen overflow-hidden p-8"}>
-    <Header/>
+  return (
+    <div className={"relative h-screen overflow-hidden p-8"}>
+      <Header />
 
-    <div className={"absolute right-10 bottom-10"}>
-      <LiveClock timeZone={"Europe/Warsaw"}/>
+      <div className={"absolute right-10 bottom-10"}>
+        <LiveClock timeZone={"Europe/Warsaw"} />
+      </div>
+
+      {renderSlogan()}
+
+      <MagneticWrapper className="absolute left-[8rem] md:left-1/3 lg:left-1/2 -translate-x-1/2 bottom-[5rem] md:bottom-4 ">
+        <ScrollDown />
+      </MagneticWrapper>
     </div>
-
-    {renderSlogan()}
-
-    <MagneticWrapper
-      className="absolute left-[8rem] md:left-1/3 lg:left-1/2 -translate-x-1/2 bottom-[5rem] md:bottom-4 ">
-      <ScrollDown/>
-    </MagneticWrapper>
-  </div>;
+  );
 }
