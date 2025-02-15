8a9a40ee100bedde391c4eb88755ec62f7b3e67d
Marcin Bojara <marcin_5@mail.ru>
2025-02-15 23:24:05 +0100

    Add Tooltip component with hover functionality
    Introduce a reusable Tooltip component that displays a title and optional image on hover. Includes customizable background color and smooth transitions, enhancing UI interactivity.

---
+++ components/ui/tooltip.tsx
@@ -0,0 +1,47 @@
+import { mergeClassNames } from "@/lib/utils";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+import Image from "next/image";
+import { type FC, useState } from "react";
+
+interface TooltipProps {
+  title: string;
+  image: string | StaticImport;
+  bgColor?: string | null;
+}
+
+const Tooltip: FC<TooltipProps> = ({ title, image, bgColor = null }) => {
+  const [isTooltipVisible, setIsTooltipVisible] = useState(false);
+
+  const renderTooltip = () => {
+    if (!isTooltipVisible) return null;
+
+    return (
+      <div
+        className="absolute -top-6 bg-black/[0.2] py-0.5 px-1.5 rounded-2xl backdrop-blur-[6px] transition-all duration-200"
+        role="tooltip"
+      >
+        <p className="font-pixel text-[10px] whitespace-nowrap">{title}</p>
+      </div>
+    );
+  };
+
+  return (
+    <div
+      className={mergeClassNames(
+        "link relative w-12 h-12 transform cursor-pointer grid place-items-center",
+        "border border-border rounded-xl hover:scale-110 transition-all duration-200",
+      )}
+      style={{ background: bgColor || "#2D2C2B" }}
+      onMouseEnter={() => setIsTooltipVisible(true)}
+      onMouseLeave={() => setIsTooltipVisible(false)}
+      aria-label={title}
+    >
+      <div className="w-[32px] h-[32px]">
+        <Image src={image} alt={title} className="w-full h-full overflow-clip object-contain" />
+      </div>
+      {renderTooltip()}
+    </div>
+  );
+};
+
+export default Tooltip;
