b3a79f23e2c428d5e006ad4624e913ec196c9a9b
Marcin Bojara <marcin_5@mail.ru>
2025-02-15 23:25:00 +0100

    Remove DeviconClass type and React className extension
    The redundant DeviconClass type and its integration with React's className attribute were removed. This cleanup simplifies typing and eliminates unused or unnecessary code.

--- @types/devicon.d.ts
+++ @types/devicon.d.ts
@@ -11,11 +11,3 @@ declare global {
 declare module "devicon" {
   export const devicon: any;
 }
-
-// For class name autocompletion (optional but useful)
-type DeviconClass = `devicon-${string}-${"plain" | "original" | "line" | "wordmark"}${"-colored" | ""}`;
-declare module "react" {
-  interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
-    className?: DeviconClass | string;
-  }
-}
