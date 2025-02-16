5c90d408074174b328426febc908e611cd48f125
Marcin Bojara <marcin_5@mail.ru>
2025-02-16 16:43:50 +0100

    Add "name" prop to Input component
    The "name" prop was introduced to enhance form handling and accessibility. This ensures that Input components can be uniquely identified and aligned with form requirements. Minor formatting adjustments were also made for readability.

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,21 +1,27 @@
-import {FC, ReactNode} from "react";
+import { FC, ReactNode } from "react";
 
 interface InputProps {
+  name: string;
   type: "text" | "password" | "email";
   placeholder: string;
   icon?: ReactNode;
 }
 
-const Input: FC<InputProps> = ({type, placeholder, icon}) => {
-  return <div className={"relative w-full"}>
-    {/* Icon */}
-    <div className={"absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none"}>
-      {icon}
+const Input: FC<InputProps> = ({ name, type, placeholder, icon }) => {
+  return (
+    <div className={"relative w-full"}>
+      {/* Icon */}
+      <div className={"absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none"}>{icon}</div>
+      <input
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        className={
+          "bg-primary-background text-primary-foreground w-full" + " rounded-lg text-sm ps-10 px-2.5 py-4 focus:outline"
+        }
+      />
     </div>
-    <input type={type} placeholder={placeholder}
-           className={"bg-primary-background text-primary-foreground w-full" +
-             " rounded-lg text-sm ps-10 px-2.5 py-4 focus:outline"}/>
-  </div>;
+  );
 };
 
 export default Input;
