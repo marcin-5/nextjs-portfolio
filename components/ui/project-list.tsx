39f67c13b37d777b7dbe31c12f11e616e6ebbd29
Marcin Bojara <marcin_5@mail.ru>
2025-02-16 12:49:24 +0100

    Incorporate the ProjectList component with its corresponding data file.
    Introduce `ProjectList` and `ProjectListItem` components for displaying project information with layout styling. Include `project.ts` to store project details such as title, description, links, and tech stack.

---
+++ components/ui/project-list.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import { FC, ReactNode } from "react";
+
+interface ProjectListProps {
+  children: ReactNode;
+}
+
+export const ProjectList: FC<ProjectListProps> = ({ children }) => {
+  return <div className="flex flex-col gap-y-6">{children}</div>;
+};
+
+interface ProjectListItemProps {
+  title: string;
+  link: string;
+  desc: string;
+  stack: string[];
+}
+
+export const ProjectListItem: FC<ProjectListItemProps> = ({ title, link, desc, stack }) => {
+  return (
+    <div className="min-h justify-start relative">
+      <Link href={link} target="_blank">
+        <LeftSide title={title} />
+        <RightSide link={link} desc={desc} stack={stack} />
+      </Link>
+    </div>
+  );
+};
+
+const LeftSide: FC<{ title: string }> = ({ title }) => (
+  <div className="h-auto flex-none break-words whitespace-pre">
+    <p className="text-secondary-foreground">{title}</p>
+  </div>
+);
+
+const RightSide: FC<{ link: string; desc: string; stack: string[] }> = ({ link, desc, stack }) => {
+  return (
+    <div className="w-auto">
+      <p className="text-primary-foreground break-words">{desc}</p>
+      <p className="text-[10px] font-normal text-neutral-400">{stack.join(", ")}</p>
+    </div>
+  );
+};
---
+++ data/project.ts
@@ -0,0 +1,22 @@
+export const projectData = [
+  {
+    title: "Portfolio",
+    link: "",
+    desc: "This page.",
+    stack: ["Next.js", "TailwindCSS", "TypeScript"],
+  },
+  {
+    title: "Cinemate",
+    link: "https://nuxt-cinemate.netlify.app/",
+    desc: "This app functions similarly to IMDb, providing movie information.",
+    stack: ["Nuxt.js", "TailwindCSS", "TypeScript"],
+  },
+  {
+    title: "Applications await deployment.",
+    link: "https://github.com/marcin-5",
+    desc: "",
+    stack: ["Source code available on GitHub."],
+  },
+];
+
+export default projectData;
