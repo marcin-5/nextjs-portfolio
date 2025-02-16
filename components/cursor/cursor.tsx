7bd58f869650fe3258df3cabe3c694a5022da938
Marcin Bojara <marcin_5@mail.ru>
2025-02-16 12:09:34 +0100

    Refactor Cursor component for improved readability.
    Updated code formatting to align with standard practices, improving readability and consistency. Adjusted `outerAlpha` property from 0 to 0.1 for better cursor visibility.

--- components/cursor/cursor.tsx
+++ components/cursor/cursor.tsx
@@ -1,25 +1,27 @@
-import {FC} from "react";
+import { FC } from "react";
 import AnimatedCursor from "react-animated-cursor";
 
 interface CursorProps {
   color: string;
 }
 
-export const Cursor: FC<CursorProps> = ({color}) => {
-  return <AnimatedCursor
-    innerSize={8}
-    outerSize={35}
-    innerScale={1}
-    outerScale={2}
-    outerAlpha={0}
-    innerStyle={{
-      backgroundColor: color,
-    }}
-    outerStyle={{
-      border: `1px solid ${color}`,
-    }}
-    clickables={["a", "button", "select", "input", ".link"]}
-  />;
+export const Cursor: FC<CursorProps> = ({ color }) => {
+  return (
+    <AnimatedCursor
+      innerSize={8}
+      outerSize={35}
+      innerScale={1}
+      outerScale={2}
+      outerAlpha={0.1}
+      innerStyle={{
+        backgroundColor: color,
+      }}
+      outerStyle={{
+        border: `1px solid ${color}`,
+      }}
+      clickables={["a", "button", "select", "input", ".link"]}
+    />
+  );
 };
 
 export default Cursor;
