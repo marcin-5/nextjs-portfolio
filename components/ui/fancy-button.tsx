9ec05816ff4ff9f9e6e33c3966b998f6e1015f04
Marcin Bojara <marcin_5@mail.ru>
2025-02-19 15:48:43 +0100

    Add anchor link to FancyButton component
    Updated the FancyButton to include an `href` attribute pointing to the "#contact" section, enabling navigation functionality. Also included minor formatting adjustments for improved code readability.

--- components/ui/fancy-button.tsx
+++ components/ui/fancy-button.tsx
@@ -1,20 +1,25 @@
-import {FC, ReactNode} from "react";
+import { FC, ReactNode } from "react";
 
 interface ButtonProps {
   text: string;
   icon: ReactNode;
 }
 
-const FancyButton: FC<ButtonProps> = ({text, icon}) => {
-  return <a className={"fancy-btn"}>
-    <div
-      className={"bg-black hover:bg-transparent text-primary-foreground hover:text-black " +
-        "group rounded-[108em] px-10 py-5 flex items-center gap-2 font-bold text-3xl " +
-        "cursor-pointer transition-all duration-75"}>
-      <span>{text}</span>
-      <span className={"group-hover:translate-x-[.75vw] transition-transform duration-100"}>{icon}</span>
-    </div>
-  </a>;
+const FancyButton: FC<ButtonProps> = ({ text, icon }) => {
+  return (
+    <a href="#contact" className={"fancy-btn"}>
+      <div
+        className={
+          "bg-black hover:bg-transparent text-primary-foreground hover:text-black " +
+          "group rounded-[108em] px-10 py-5 flex items-center gap-2 font-bold text-3xl " +
+          "cursor-pointer transition-all duration-75"
+        }
+      >
+        <span>{text}</span>
+        <span className={"group-hover:translate-x-[.75vw] transition-transform duration-100"}>{icon}</span>
+      </div>
+    </a>
+  );
 };
 
 export default FancyButton;
