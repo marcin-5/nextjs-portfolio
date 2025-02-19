9341ca7bf96ec8b4b56f951f6ef688fa0563d904
Marcin Bojara <marcin_5@mail.ru>
2025-02-19 19:07:05 +0100

    Add full-screen menu component for navigation
    Introduce a `FullScreenMenu` component with animations using Framer Motion. The component includes profile, navigation links, and footer links, enhancing the navigation experience. This structure provides a visually engaging and responsive menu layout.

---
+++ components/navigation/header/full-screen-menu/full-screen-menu.tsx
@@ -0,0 +1,48 @@
+import { menuSlide } from "@/components/navigation/header/full-screen-menu/animation";
+import { MAIN_NAV_ITEMS } from "@/components/navigation/header/full-screen-menu/config";
+import { FooterLinks } from "@/components/navigation/header/full-screen-menu/footer-links";
+import MenuCard from "@/components/navigation/header/full-screen-menu/menu-card";
+import NavLink from "@/components/navigation/header/full-screen-menu/nav-link";
+import Profile from "@/components/ui/profile";
+import { motion } from "framer-motion";
+
+export default function FullScreenMenu({ onClose }: { onClose: () => void }) {
+  return (
+    <motion.div
+      variants={menuSlide}
+      initial="initial"
+      animate="enter"
+      exit="exit"
+      className="h-screen w-full bg-slate-950 fixed top-0 right-0 text-primary-foreground z-40 font-oswald"
+    >
+      <ProfileSection />
+      <MainContent onClose={onClose} />
+      <FooterLinks />
+    </motion.div>
+  );
+}
+
+const ProfileSection = () => (
+  <div className="relative w-full pl-[5%]">
+    <div className="absolute top-8">
+      <Profile />
+    </div>
+  </div>
+);
+
+const MainContent = ({ onClose }: { onClose: () => void }) => (
+  <div className="absolute bottom-32 w-full px-[5%]">
+    <div className="grid relative" style={{ gridTemplateColumns: "1fr 500px" }}>
+      <NavigationLinks items={MAIN_NAV_ITEMS} onClose={onClose} />
+      <MenuCard onClose={onClose} />
+    </div>
+  </div>
+);
+
+const NavigationLinks = ({ items, onClose }: { items: typeof MAIN_NAV_ITEMS; onClose: () => void }) => (
+  <div className="pl-4 flex flex-col justify-end">
+    {items.map((item, index) => (
+      <NavLink key={index} {...item} index={index} dotEffect={true} onClick={onClose} />
+    ))}
+  </div>
+);
