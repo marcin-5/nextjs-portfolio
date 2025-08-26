e7bbb2a067c2e2b6803dd6885bd2c32f61b1fff2
Marcin Bojara <marcin_5@mail.ru>
2025-08-26 19:26:32 +0200

    Refactor `stack.tsx`: improve layout responsiveness, enhance spacing, and restructure DOM for better readability and flexibility

--- components/cards/stack.tsx
+++ components/cards/stack.tsx
@@ -7,15 +7,18 @@ export default function StackCard() {
     <Card title="My Tech stack" sectionId={"stack"}>
       <div className="flex flex-col gap-6 mt-2">
         {stackData.map((tech, index) => (
-          <div key={index} className="grid items-center gap-[90px]" style={{ gridTemplateColumns: "50px 1fr" }}>
-            {/* Stack group name*/}
-            <div className="h-auto flex-none break-words whitespace-pre">
+          <div
+            key={index}
+            className="grid grid-cols-1 gap-y-2 sm:[grid-template-columns:50px_1fr] sm:items-center sm:gap-x-12"
+          >
+            {/* Stack group name */}
+            <div className="h-auto flex-none break-words whitespace-normal sm:whitespace-pre">
               <p className="text-secondary-foreground">{tech.title}</p>
             </div>
-            {/* Tooltip */}
-            <div className="flex gap-4">
-              {tech.stack.map((tooltip, index) => (
-                <Tooltip key={index} {...tooltip} />
+            {/* Tooltip list */}
+            <div className="flex flex-wrap gap-3 sm:gap-4 overflow-visible">
+              {tech.stack.map((tooltip, i) => (
+                <Tooltip key={i} {...tooltip} />
               ))}
             </div>
           </div>
