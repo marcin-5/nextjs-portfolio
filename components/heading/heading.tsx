8fa457fbcf758f46ae3a9a3c9a2f2e64f4cd9dc2
Marcin Bojara <marcin_5@mail.ru>
2025-08-26 18:55:12 +0200

    Refactor `Heading`: improve spacing consistency, fix typos in class names, and update typography for better responsiveness and readability

--- components/heading/heading.tsx
+++ components/heading/heading.tsx
@@ -1,6 +1,6 @@
-import {HeadingAnimatedSvg} from "@/components/heading/heading-animated-svg";
+import { HeadingAnimatedSvg } from "@/components/heading/heading-animated-svg";
 import SvgCurve from "@/components/visualEffects/svg-curve";
-import {FC} from "react";
+import { FC } from "react";
 
 interface HeadingProps {
   number: string;
@@ -8,31 +8,27 @@ interface HeadingProps {
   title_2: string;
 }
 
-const Heading: FC<HeadingProps> = ({number, title_1, title_2}) => {
+const Heading: FC<HeadingProps> = ({ number, title_1, title_2 }) => {
   return (
-    <div className="relative my-10 px-8 z-20">
+    <div className="relative px-8 z-20">
       {/* Number */}
-      <div
-        className="outline-none flex flec-col justify-start shrink-0 opacity-5 transform -top-32 2xl:-top-24 w-[71px] flex-none h-auto left-4 lg:left-12 absolute whitespace-pre">
+      <div className="outline-none flex flex-col justify-start shrink-0 opacity-5 transform -top-32 2xl:-top-24 w-[71px] flex-none h-auto left-4 lg:left-12 absolute whitespace-pre">
         <h2 className="font-pixel text-[180px] text-center text-primary-foreground relative">
-          <span className="bottom_fade bg-clip-text text-transparent p-4">
-            {number}
-          </span>
+          <span className="bottom_fade bg-clip-text text-transparent p-4">{number}</span>
         </h2>
       </div>
       {/* Heading text wrapper */}
       <div className="flex items-center flex-nowrap min-h-min overflow-hidden p-0 w-full font-oswald">
-        <p className="text-[17vw] lg:text-[12vw] leading-[100%] text-primary-foreground mr-3">
+        <p className="text-[clamp(2rem,10vw,5.5rem)] lg:text-[clamp(3rem,7vw,7.5rem)] leading-tight text-primary-foreground mr-3">
           {title_1}
         </p>
-        <HeadingAnimatedSvg text=" LEARN MORE ABOUT MY PROJECTS "/>
-        <p className="text-[17vw] lg:text-[12vw] leading-[100%] text-primary-foreground italic">
+        <HeadingAnimatedSvg text=" LEARN MORE ABOUT MY PROJECTS " />
+        <p className="text-[clamp(2rem,10vw,5.5rem)] lg:text-[clamp(3rem,7vw,7.5rem)] leading-tight text-primary-foreground italic">
           {title_2}
         </p>
       </div>
 
-      <SvgCurve/>
-
+      <SvgCurve />
     </div>
   );
 };
