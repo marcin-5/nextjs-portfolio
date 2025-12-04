4a2f85b9904704a6d5984d8d9829991c13fdba97
Marcin Bojara <marcin_5@mail.ru>
2025-12-04 16:34:22 +0100

    Reorder and clean imports in `layout.tsx`, apply minor formatting changes, and add `suppressHydrationWarning` to `<html>` tag.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
-import type {Metadata} from "next";
-import {Oswald, Signika} from "next/font/google";
-import "./globals.css";
-import {mergeClassNames as mcn} from "@/lib/utils";
-import localFont from "next/font/local";
-import GrainEffect from "@/components/visualEffects/grain-effect";
 import Cursor from "@/components/cursor/cursor";
+import GrainEffect from "@/components/visualEffects/grain-effect";
+import { mergeClassNames as mcn } from "@/lib/utils";
+import type { Metadata } from "next";
+import { Oswald, Signika } from "next/font/google";
+import "./globals.css";
+import localFont from "next/font/local";
 
 const LANGUAGE = "en";
 
 // Fonts
-const mainFont = Signika({subsets: ["latin"]});
-const oswaldFont = Oswald({subsets: ["latin"], variable: "--font-oswald"});
+const mainFont = Signika({ subsets: ["latin"] });
+const oswaldFont = Oswald({ subsets: ["latin"], variable: "--font-oswald" });
 const pixelFont = localFont({
   src: "../public/assets/fonts/pixel_font-7.ttf",
   variable: "--font-pixel",
@@ -23,17 +23,17 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-                                     children,
-                                   }: Readonly<{
+  children,
+}: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang={LANGUAGE}>
-    <body className={mcn(mainFont.className, oswaldFont.variable, pixelFont.variable)}>
-    <GrainEffect/>
-    <Cursor color={"#fff"}/>
-    {children}
-    </body>
+    <html lang={LANGUAGE} suppressHydrationWarning>
+      <body className={mcn(mainFont.className, oswaldFont.variable, pixelFont.variable)}>
+        <GrainEffect />
+        <Cursor color={"#fff"} />
+        {children}
+      </body>
     </html>
   );
 }
